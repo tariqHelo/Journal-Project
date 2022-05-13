130db2ae5838d76c54127388b1a216905eebb9e9
unknown <tariq.1994t12@gmail.com>
2022-05-14 01:44:27 +0300

    Win

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,45 +10,51 @@ class DashboardController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Support\Collection
      */
     public function index()
     {   
-       $t = Carbon::now();
+        $t = Carbon::now();
        $data = Journal::query()->get()->groupBy(function($t) {
             return Carbon::parse($t->entry_date)->format('Y-m-d');
         });
-
-      // dd($data->toArray());
        $events = collect($data)->values()->map(function ($events)  {
             return  [
-                   'title'     =>  $events->count(),
-                   'start'     => ($events[0]->entry_date),
-                  'className' => "fc-event-danger fc-event-solid-warning",
-                  //count of tarde // Done
-                  //pnl sum all profit
-                  //win rate 
-            ];
-        });
-       
-       $pnl = collect($data)->values()->map(function ($events)  {
-            return  [
-                   'title'     =>  $events->sum('profit'),
+                   'title'     =>  'Count'.':' .' '.$events->count(),
                    'start'     => ($events[0]->entry_date),
                    'className' => "fc-event-danger fc-event-solid-warning",
+            ];
+        });
+       $pnl = collect($data)->values()->map(function ($pnl)  {
+            return  [
+                   'title'     =>   'PnL'.':' .' $'.$pnl->sum('profit'),
+                   'start'     => ($pnl[0]->entry_date),
+                   'className' => "fc-event-light fc-event-solid-primary",
                   //count of tarde // Done
-                  //pnl sum all profit
+                  //pnl sum all profit //Done
                   //win rate 
             ];
         });
+        $rate = collect($data)->values()->map(function ($rate)  {
+            return  [
+                    'title'     =>  'Win Rate'.':'. ' '.$rate->count() / $rate->sum('profit').'%',
+                //    'title'     =>   'Win Rate'.':'. ' '.$rate->avg('profit') .'%',
+                   'start'     => ($rate[0]->entry_date),
+                   'className' => "fc-event-solid-info fc-event-light",
+                  //count of tarde // Done
+                  //pnl sum all profit //Done
+                  //win rate //Done
+            ];
+        });
 
-    dd($pnl);
+     $allItems = $events->merge($pnl)->merge($rate);
+     dd($allItems);
+     return response()->json(['events' => $allItems]);
+     //win rate => // % of Quantity positive Trades vs Negative Trades (Positive Profit vs Negative Profit) 
+             // Count of 5 Trades have positive Profit,
+             // Count of 5 Trades have negative Profit —>
+             // Win Rate = 50% // Same for the Calendar, just for the specific Date 
 
-
-
-      ///  return response()->json(['events' => $events]);
-    //    return view('admin.calender.index',
-    // ['events' => $events]);  
     }
 
     /**
